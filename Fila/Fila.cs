4f327723d89c541e5c53eb434c6c53c91d6010d6
gafornazari <fornazari.ga@gmail.com>
2025-10-15 15:38:23 -0300

    Add project files.

---
+++ Fila/Fila.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilaEx
+{
+    public class Fila
+    {
+        Pessoa head;
+        Pessoa tail;
+
+
+        public void CriarFila()
+        {
+            Pessoa head = null;
+            Pessoa tail = head;
+        }
+
+        public bool FilaVazia()
+        {
+            return head == null;
+        }
+
+        public void InserirPessoa(Pessoa pessoa)
+        {
+            if (FilaVazia())
+            {
+                head = pessoa;
+                tail = pessoa;
+                pessoa.Proximo = null;
+            }
+            else
+            {
+                tail.Proximo = pessoa;
+                tail = pessoa;
+            }
+
+            Console.WriteLine("Inserido(a)!");
+        }
+
+        public void RemoverPessoa()
+        {
+            if (FilaVazia())
+            {
+                Console.WriteLine("\nA fila está vazia, não há pessoas para remover!");
+                return;
+            }
+            else
+            {
+                head = head.Proximo;
+                if (head == null)
+                    tail = null;
+            }
+            Console.WriteLine("Removido(a)!");
+        }
+
+        public int TamanhoFila()
+        {
+            Pessoa aux = head;
+            int cont = 0;
+
+            while (aux != null)
+            {
+                cont++;
+                aux = aux.Proximo;
+            }
+
+            return cont;
+        }
+
+        public void ImprimirFila()
+        {
+            if (FilaVazia())
+            {
+                Console.WriteLine("\nA fila está vazia!");
+                return;
+            }
+            else
+            {
+                Pessoa aux = head;
+                while (aux != null)
+                {
+                    Console.WriteLine(aux.Nome);
+                    aux = aux.Proximo;
+                }
+            }
+        }
+    }
+}
---
+++ Fila/Program.cs
@@ -0,0 +1,45 @@
+﻿using FilaEx;
+
+Fila fila = new Fila();
+int op = 1;
+
+fila.CriarFila();
+
+do
+{
+
+    Console.WriteLine("\n\nO que você deseja fazer?");
+    Console.WriteLine("1 - Inserir pessoa na fila");
+    Console.WriteLine("2 - Remover pessoa da fila");
+    Console.WriteLine("3 - Imprimir a fila");
+    Console.WriteLine("4 - Saber o tamanho a fila");
+    Console.WriteLine("0 - Sair");
+    Console.Write("Opção: ");
+    op = int.Parse(Console.ReadLine());
+
+    switch (op)
+    {
+        case 1:
+            Pessoa pessoa = new Pessoa();
+            Console.WriteLine("Qual o nome da pessoa?");
+            pessoa.Nome = Console.ReadLine();
+            fila.InserirPessoa(pessoa);
+            break;
+        case 2:
+            fila.RemoverPessoa();
+            break;
+        case 3:
+            Console.WriteLine("Fila:");
+            fila.ImprimirFila();
+            break;
+        case 4:
+            Console.Write("Tamanho da fila: ");
+            Console.WriteLine(fila.TamanhoFila()); 
+            break;
+        default:
+            if (op != 0)
+                Console.WriteLine("Opção inválida!");
+            break;
+    }
+
+} while (op != 0);
